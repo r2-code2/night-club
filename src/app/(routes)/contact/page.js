875bc198c5839d738af7bddae7f2cc485dab8bb3
Ragnhild <ragu0002@stud.kea.dk>
2025-12-03 17:02:03 +0100

    ragnhild-add-header-contact

--- src/app/(routes)/contact/page.js
+++ src/app/(routes)/contact/page.js
@@ -1,11 +1,12 @@
 import ContactForm from "@/app/components/contact/o-form/ContactForm";
 import Footer from "@/app/components/footer/Footer";
-import { Caption, Subheading, HeadingSecondary, HeadingMain, HeadingXL } from "@/app/components/typography";
+import Header from "@/app/components/header/Header";
 
 export default function Contact() {
   return (
     <>
-      <main className="grid col-(--content-col) grid-cols-subgrid">
+      <Header />
+      <main className="grid col-(--content-col) grid-cols-subgrid mt-30">
         <ContactForm />
       </main>
       <Footer />

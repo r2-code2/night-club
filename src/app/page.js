6223bdc21f33b4b9f6712fe54f259bd94f3121fa
Ragnhild <ragu0002@stud.kea.dk>
2025-12-05 13:54:27 +0100

    ragnhild-create-newsletter-form

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Events from "./components/home/events/Events";
 import Gallery from "./components/home/gallery/Gallery";
 import RecentBlog from "./components/home/recentblog/RecentBlog";
 import Testimonials from "./components/testimonials/Testimonials";
-
+import NewsLetter from "./components/home/newsletter/NewsLetter";
 
 export default function Home() {
   return (
@@ -11,10 +11,10 @@ export default function Home() {
       <main className="grid col-(--full-col) grid-cols-subgrid">
         <HomeHero />
         <Events />
-
         <Gallery />
-    <Testimonials />
-         <RecentBlog />
+        <Testimonials />
+        <RecentBlog />
+        <NewsLetter />
       </main>
     </>
   );

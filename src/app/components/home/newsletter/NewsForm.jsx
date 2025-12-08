f9177df4de897baba740ff3cdb7bff2b21845e00
Ragnhild <ragu0002@stud.kea.dk>
2025-12-08 11:42:03 +0100

    ragnhild-add-react-hook-forms-and-zod

--- src/app/components/home/newsletter/NewsForm.jsx
+++ src/app/components/home/newsletter/NewsForm.jsx
@@ -1,11 +1,30 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import MainButton from "../../buttons/MainButton";
+import { Caption } from "../../typography";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
 
 const NewsForm = () => {
+  const form = useForm({ resolver: zodResolver(formSchema) });
+  const { register, handleSubmit, formState } = form;
+  const { errors, isSubmitting } = formState;
+  const onSubmit = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("Form has been submittet", data);
+  };
   return (
-    <form className="grid gap-3 md:flex md:justify-self-center md:w-2/3 md:gap-5">
-      <input type="text" defaultValue="Enter Your Email" className="p-2 border-b md:basis-3/4" />
-      <MainButton text="Subscribe" styling="w-1/2 justify-self-center md:basis-1/4" />
-    </form>
+    <>
+      <Caption color="text-red-500" text={errors.email?.message} />
+      <form className="grid gap-3 md:flex md:justify-self-center md:w-2/3 md:gap-5" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" id="email" placeholder="Enter Your Email" className="p-2 border-b md:basis-3/4 focus:outline-accent placeholder:text-foreground" {...register("email")} />
+        <MainButton disabled={isSubmitting} text={` ${isSubmitting ? "Subscribing..." : "Subscribe"} `} styling="w-1/2 justify-self-center md:basis-1/4" />
+      </form>
+    </>
   );
 };
 

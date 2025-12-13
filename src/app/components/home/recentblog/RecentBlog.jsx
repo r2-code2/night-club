89a7725044aeba1b84198363a0ad4e97ef91d712
Ragnhild <ragu0002@stud.kea.dk>
2025-12-13 15:20:29 +0100

    ragnhild-add-background

--- src/app/components/home/recentblog/RecentBlog.jsx
+++ src/app/components/home/recentblog/RecentBlog.jsx
@@ -2,16 +2,19 @@
 
 import { HeadingMain } from "../../typography";
 import RescentBlogPost from "./RecentBlogPost";
+import PatternBg from "../../bgOverlays/PatternBg";
 
 const RecentBlog = () => {
   return (
     <section className="col-(--content-col)">
-      <div className=" h-50 flex items-center">
-        <HeadingMain color="white" text="Recent blog" />
-      </div>
-      <div className="grid md:flex gap-10 md:gap-5">
-        <RescentBlogPost />
-      </div>
+      <PatternBg>
+        <div className=" h-50 flex items-center">
+          <HeadingMain color="white" text="Recent blog" />
+        </div>
+        <div className="grid md:flex gap-10 md:gap-5">
+          <RescentBlogPost />
+        </div>
+      </PatternBg>
     </section>
   );
 };

e7f34f6fe0862a559c322c674f6022034c1416cb
Ragnhild <ragu0002@stud.kea.dk>
2025-12-18 10:51:09 +0100

    ragnhild-tutorial-upgrades

--- src/app/(routes)/book/page.js
+++ src/app/(routes)/book/page.js
@@ -2,8 +2,22 @@ import Header from "@/app/components/header/Header";
 import HeroSection from "@/app/components/herosection/HeroSection";
 import BookTableOverview from "@/app/components/book/BookTableOverview";
 import ErrorMessages from "@/app/components/errormessages/ErrorMessages";
+import { Suspense } from "react";
 
 export default async function BookTable() {
+  return (
+    <>
+      <Header />
+      <main className="grid col-(--full-col) grid-cols-subgrid">
+        <HeroSection text="Book table" />
+        <Suspense fallback={<div>Loading ...</div>}>
+          <FetchTable></FetchTable>
+        </Suspense>
+      </main>
+    </>
+  );
+}
+const FetchTable = async () => {
   try {
     const response = await fetch("http://localhost:4000/reservations");
     const data = await response.json();
@@ -11,15 +25,10 @@ export default async function BookTable() {
       return <ErrorMessages message="We´re having some trouble loading this data, try again later!" />;
     }
     const takenTables = data.map((r) => String(r.table));
-    const date = data.map((r) => r.date);
 
     return (
       <>
-        <Header />
-        <main className="grid col-(--full-col) grid-cols-subgrid">
-          <HeroSection text="Book table" />
-          <BookTableOverview takenTables={takenTables} />
-        </main>
+        <BookTableOverview takenTables={takenTables} />
       </>
     );
   } catch (error) {
@@ -27,12 +36,8 @@ export default async function BookTable() {
 
     return (
       <>
-        <Header />
-        <main className="grid col-(--full-col) grid-cols-subgrid">
-          <HeroSection text="Book table" />
-          <ErrorMessages message="We’re having some trouble loading this data, try again later!" />
-        </main>
+        <ErrorMessages message="We’re having some trouble loading this data, try again later!" />
       </>
     );
   }
-}
+};
